8c2068f85bd66ef699009c90dfaed20dbf5b4a79
TiagoSG22 <Teizokurei@gmail.com>
2016-03-29 21:57:06 -0300

    Update DaoBase.java
    Ajustes!

--- src/dao/DaoBase.java
+++ src/dao/DaoBase.java
@@ -5,8 +5,6 @@ public abstract class DaoBase<T> {
 	
 	protected static final String FORNECEDOR = "fornecedor";
 	protected static final String PESSOA = "pessoa";
-	protected static final String PRODUTO = "produto";
-	protected static final String VENDA = "venda";
 	protected static final String DATABASE = "test";
 	
 	public abstract T save(T obj);
